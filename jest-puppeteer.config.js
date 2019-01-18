072b9f9758228347c2a06f44585a28ed57094fef
Elina Åberg <elina.aberg@hotmail.com>
2019-01-18 11:14:27 +0100

    Deleted unused folders

--- jest-puppeteer.config.js
+++
@@ -1,9 +0,0 @@
-const DEBUG = ["debug", "dev", "development"].includes(process.env.NODE_ENV);
-
-module.exports = {
-	launch: {
-		headless: !DEBUG,
-		devtools: !DEBUG,
-		slowMo: DEBUG ? 50 : 0
-	}
-};
